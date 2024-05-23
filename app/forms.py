899561b628d57071dad69338126ad5f029aa4706
zyloxweeb <giuseppefattor@gmail.com>
2024-05-23 21:04:57 +0200

    reset

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField
-from wtforms.validators import DataRequired
-
-class ProdottoForm(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired()])
-    quantita = IntegerField('Quantità', validators=[DataRequired()])
-    scadenza = DateField('Data di Scadenza', validators=[DataRequired()])
-    lotto = StringField('Lotto', validators=[DataRequired()])
--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dawdaw dada'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///magazzino.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
